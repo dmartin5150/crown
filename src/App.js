7996b4533bad21d6c3f149396057873d97971e34
David Martin <dmartin5150@icloud.com>
2022-11-06 18:38:52 -0600

    added signin to navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./categories.styles.scss";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation";
 import Home from "./routes/home/home";
+import SignIn from "./routes/sign-in/sign-in";
 
 
 const Shop = () => {
@@ -16,7 +17,8 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
