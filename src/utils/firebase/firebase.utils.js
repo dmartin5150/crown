db6a58f108b98be3b2d55b3cc9d822c7b3afb474
David Martin <dmartin5150@icloud.com>
2022-11-06 21:45:41 -0600

    added sign up with email and password

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from "firebase/auth";
 
 import {
@@ -32,10 +33,12 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
+    if (!userAuth) return;
     const userDocRef =  doc(db,'users',userAuth.uid);
     console.log(userDocRef);
 
@@ -50,7 +53,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             })
         }catch(error) {
             console.log('Error creating user', error)
@@ -59,11 +63,10 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
     return userDocRef;
 
+};
 
-    //if user data does not exist
-    
-    
-    //if user data exists
-    //return docref
+export const createAuthUserWithEmailAndPassword = async (email,password) => {
+    if (!email || !password) return;
 
+    return await createUserWithEmailAndPassword(auth, email,password);
 }
