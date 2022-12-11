d3b1b7fe7cb1f2a8cdeecce4aa97a4b2001e09d0
David <dmartin5150@icloud.com>
2022-12-11 11:35:13 -0600

    adding stripe

--- src/components/payments-form/payment-form.jsx
+++ src/components/payments-form/payment-form.jsx
@@ -15,11 +15,21 @@ const PaymentForm = () => {
       return;
     }
 
+    const response = await fetch('./netlify/functions/create-payment-intent.js', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({amount: 10000})
+    }).then(res => res.json());
+
+    console.log(response);
+
 
   }
 
   return (
-    <PaymentFormContainer>
+    <PaymentFormContainer onSubmit={paymentHandler}>
       <FormContainer>
         <h2>Credit Card Payment:</h2>
         <CardElement />
